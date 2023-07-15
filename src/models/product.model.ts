f8f3fb47dda2dee9996433a9873d78e56f875e18
developerdesigner18 <developer.designer18@gmail.com>
2023-07-15 12:12:05 +0530

    Add: Product Schema mongoose model

---
+++ src/models/product.model.ts
@@ -0,0 +1,50 @@
+import { Document, Schema, model } from "mongoose";
+
+// Type Defining
+export interface IProduct extends Document {
+  productName: string;
+  productImage: string;
+  productDescription: string;
+  productCategory: string;
+  productQuantity: number;
+  productExpectedSale: number;
+  productPrice: number;
+  totalSoldQty: number;
+}
+
+const ProductSchema = new Schema<IProduct>({
+  productName: {
+    type: String,
+    required: true,
+  },
+  productImage: {
+    type: String,
+    required: true,
+  },
+  productDescription: {
+    type: String,
+    required: true,
+  },
+  productCategory: {
+    type: String,
+    required: true,
+  },
+  productQuantity: {
+    type: Number,
+    required: true,
+  },
+  productExpectedSale: {
+    type: Number,
+    required: true,
+  },
+  productPrice: {
+    type: Number,
+    required: true,
+  },
+  totalSoldQty: {
+    type: Number,
+    required: true,
+  },
+});
+
+export const UserModel = model<IProduct>("ProductSchema", ProductSchema);
