55c1cf0aca1a784b8d5796c6c8302e9fde8d4728
developerdesigner18 <developer.designer18@gmail.com>
2023-07-17 17:42:45 +0530

    Added pieChart and HeatMap

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,20 +1,29 @@
-import { gql } from 'apollo-server';
+const {gql} = require('apollo-server')
 const user = require('../models/user.model')
 const product = require('../models/product.model')
 const typeDefs= gql `
 
 type category {
     categoryName: String,
-    totalSoldQty: Int
+    number: Int
 }
 
 type pieChart {
-    [category]
+    category:[category]
 }
 
+type country {
+    countryName: String,
+    number: Int
+}
+
+type heatMap {
+    country:[country]
+}
 
 type Query{
     getPieChartData: pieChart!
+    getHeatMapData: heatMap!
 }
 
 `;
@@ -24,7 +33,42 @@ type Query{
 const resolvers ={  
     Query:{
         getPieChartData:async (_: any) => {
-            const data = await product.find
+            const data = await product.find();
+            let category:any = [];
+            
+            data.forEach((element:any) => {
+              const existingCategory = category.find((re:any) => re.categoryName === element.productCategory);
+            
+              if (existingCategory) {
+                existingCategory.number += 1;
+              } else {
+                category.push({
+                  categoryName: element.productCategory,
+                  number: 1,
+                });
+              }
+            });
+            return { category: category };
+            
+        },
+        getHeatMapData:async (_: any) => {
+            const data = await user.find();
+            let category:any = [];
+            
+            data.forEach((element:any) => {
+              const existingCategory = category.find((re:any) => re.countryName === element.country);
+            
+              if (existingCategory) {
+                existingCategory.number += 1;
+              } else {
+                category.push({
+                  countryName: element.country,
+                  number: 1,
+                });
+              }
+            });
+            return { country: category };
+            
         },
     }
 }
